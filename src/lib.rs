0ce77587a191701bbf493f0fb01252781253289a
Heinz N. Gies <heinz@licenser.net>
2017-07-04 16:27:42 +0200

    do not return result but just err

--- src/lib.rs
+++ src/lib.rs
@@ -115,9 +115,9 @@ impl<T> Failure<T> {
     pub fn new(state: T, error: Box<Error>) -> Self {
         Failure { state, error }
     }
-    /// Truns the Failure into an result / Err
-    pub fn to_error(self) -> Result<T, Box<Error>> {
-        Err(self.error)
+    /// Truns the Failure into an Err
+    pub fn to_error(self) -> Box<Error> {
+        self.error
     }
 }
 
